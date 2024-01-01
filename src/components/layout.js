21e45a6279731b4c14ee8f398724a1f383b3df36
djlee <moovinzoo@gmail.com>
2024-01-01 18:13:46 +0900

    feat: apply styles by using styled-components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,45 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  margin: auto;
+  max-width: 500px;
+  font-family: sans-serif;
+`;
+
+const Heading = styled.h1`
+  color: rebeccapurple;
+`;
+
+const NavLinks = styled.ul`
+  display: flex;
+  list-style: none;
+  padding-left: 0;
+`;
+
+const NavLinkItem = styled.li`
+  padding-right: 2rem;
+`;
+
+const NavLinkText = styled(Link)`
+  color: black;
+`;
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div>
+    <Container>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
+        <NavLinks>
+          <NavLinkItem><NavLinkText to="/">Home</NavLinkText></NavLinkItem>
+          <NavLinkItem><NavLinkText to="/about">About</NavLinkText></NavLinkItem>
+        </NavLinks>
       </nav>
       <main>
-        <h1>{pageTitle}</h1>
+        <Heading>{pageTitle}</Heading>
         {children}
       </main>
-    </div>
+    </Container>
   )
 }
 

b24f692b4c57508101671d9628dec82c5d9dc713
djlee <moovinzoo@gmail.com>
2024-01-02 15:32:17 +0900

    refactor: gatsby-filesystem to source ./blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,22 +6,29 @@ module.exports = {
     title: `My Personal Web`,
     siteUrl: `https://moovinzoo.github.io`
   },
-  plugins: ["gatsby-plugin-styled-components", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", {
-    // Add a collection called "posts" that looks for files in content/posts
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      name: `posts`,
-      path: `./content/posts`,
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    },
+    "gatsby-plugin-mdx",
+    {
+      // Add a collection called "posts" that looks for files in content/posts
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        // name: `posts`,
+        // path: `./content/posts`,
+        name: `blog`,
+        path: `./blog`,
+      },
     },
 //    options: {
 //      "name": "pages",
 //      "path": "./src/pages/"
 //    },
 //    __key: "pages"
-  }]
+  ],
 };
